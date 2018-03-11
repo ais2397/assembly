730cf4db53bc9ce821968ac8c4c1394f0754dd9b
ais2397 <35501599+ais2397@users.noreply.github.com>
2018-03-11 10:25:12 +0530

    Add files via upload

---
+++ ques4.asm
@@ -0,0 +1,56 @@
+extern printf
+extern scanf
+
+section .data
+	n: dd 1 ,0
+	N: dd 0
+	in: db "%d" ,0
+	msg1: db "Enter the number ",10,0
+	msg2: db "The number is a prime no. ",0
+	msg3: db "The number is not a prime no. ",0
+
+section .text
+	global main
+	main:
+	push ebp
+	mov ebp,esp
+
+	push msg1
+	call printf
+
+	push N
+	push in
+	call scanf
+
+	mov ecx,[n]
+
+	check:
+	inc ecx
+	mov edx,0
+	mov eax, [N]
+	mov ebx, ecx
+	div ebx
+
+	cmp ebx,[N]
+	je print2
+
+	cmp edx, 0
+	jne check
+	jmp print3
+
+
+	print2:  ;prime no.
+
+	push msg2
+	call printf
+	jmp exit
+
+	print3:	;not a prime no.
+
+	push msg3
+	call printf
+
+	exit:
+	leave
+	ret
+
